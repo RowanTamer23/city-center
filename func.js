2eea7a3605f433817f7a6d972b1357e982a63ed9
RowanTamer23 <rowan.tamer232001@gmail.com>
2023-06-12 03:01:20 +0300

    CityCenter

---
+++ func.js
@@ -0,0 +1,4 @@
+document.getElementById("lost").innerHTML =
+  '<a href="tel:' + 1273722974 + '"> lost</a>';
+document.getElementById("security").innerHTML =
+  '<a href="tel:' + 1030160168 + '"> security</a>';
