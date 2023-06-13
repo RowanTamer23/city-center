34fe2117ef0a00d1833479b5a3f50677bc055f14
RowanTamer23 <rowan.tamer232001@gmail.com>
2023-06-14 01:37:11 +0300

    f1

--- func.js
+++ func.js
@@ -1,4 +1,21 @@
-document.getElementById("lost").innerHTML =
-  '<a href="tel:' + 01273722974 + '"> lost</a>';
-document.getElementById("security").innerHTML =
-  '<a href="tel:' + 01030160168 + '"> security</a>';
+function calllost() {
+  f1();
+  f2();
+}
+function f1() {
+  window.open("tel:01273722974");
+}
+function f2() {
+  window.open("https://api.whatsapp.com/send?phone=01273722974&text=%20");
+}
+
+function callsec() {
+  f11();
+  f12();
+}
+function f11() {
+  window.open("tel:01030160168");
+}
+function f12() {
+  window.open("https://api.whatsapp.com/send?phone=01030160168&text=%20");
+}
