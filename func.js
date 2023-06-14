3159f37757dd84b9411d2642b86313342fb2c24f
RowanTamer23 <rowan.tamer232001@gmail.com>
2023-06-14 03:15:36 +0300

    final

--- func.js
+++ func.js
@@ -1,7 +1,21 @@
 function calllost() {
-  f1();
-  f2();
+  document.getElementById("new11").innerHTML = "lost and found";
+
+  var news = document.createElement("button");
+  news.innerHTML = "whatsapp";
+  news.setAttribute("class", "butt");
+  news.setAttribute("onclick", "f2()");
+  document.getElementById("new1").appendChild(news);
+
+  var neww = document.createElement("button");
+  neww.innerHTML = "call";
+  neww.setAttribute("class", "butt");
+  neww.setAttribute("onclick", "f1()");
+  document.getElementById("new1").appendChild(neww);
+
+  document.getElementById("lost").remove();
 }
+
 function f1() {
   window.open("tel:01273722974");
 }
@@ -10,8 +24,20 @@ function f2() {
 }
 
 function callsec() {
-  f11();
-  f12();
+  document.getElementById("new22").innerHTML = "security control";
+  var news = document.createElement("button");
+  news.innerHTML = "whatsapp";
+  news.setAttribute("class", "butt");
+  news.setAttribute("onclick", "f12()");
+  document.getElementById("new2").appendChild(news);
+
+  var neww = document.createElement("button");
+  neww.innerHTML = "call";
+  neww.setAttribute("class", "butt");
+  neww.setAttribute("onclick", "f11()");
+  document.getElementById("new2").appendChild(neww);
+
+  document.getElementById("security").remove();
 }
 function f11() {
   window.open("tel:01030160168");
